a51c50da070390c203466ac28fddc6bee9f164fd
Felipe Mayorga <fmayorga@360pmi.com>
2023-10-31 08:52:31 -0500

    Set up project to Strapi cloud

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,4 @@
 module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: "aws-s3",
-      providerOptions: {
-        baseUrl: env("CDN_URL"),
-        s3Options: {
-          accessKeyId: env("AWS_ACCESS_KEY_ID"),
-          secretAccessKey: env("AWS_ACCESS_SECRET"),
-          region: env("AWS_REGION"),
-          params: {
-            ACL: env("AWS_ACL", "public-read"),
-            Bucket: env("AWS_BUCKET"),
-          },
-        },
-      },
-    },
-  },
   "record-locking": {
     enabled: true,
   },
